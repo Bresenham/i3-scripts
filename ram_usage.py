dd5a5f74b5d9ac7ecde5e87915b4410b383724f1
Bresenham <gaul@hm.edu>
2019-10-18 12:47:56 +0200

    Updated config, removed unused ram_usage.py

--- ram_usage.py
+++
@@ -1,8 +0,0 @@
-import psutil
-
-d = dict(psutil.virtual_memory()._asdict())
-
-total = d["total"] / (1024 * 1024 * 1024)
-used = d["used"] / (1024 * 1024 * 1024)
-
-print("RAM: {:.2f}/{:.2f}GiB".format(used, total))
